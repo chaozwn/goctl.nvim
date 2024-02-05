33635c47a7583b2a827c6609b2d79149eb7c8ec9
zhao_wei_nan <326747337@qq.com>
2024-02-05 16:43:41 +0800

    feat: update

--- lua/goctl/common.lua
+++ lua/goctl/common.lua
@@ -1,24 +1,6 @@
 local M = {}
 
 local job = require("goctl.job")
-local notify = require("goctl.notify")
-
----Check goctl binary
-function M.goctl_check()
-	local status = false
-	local gopath = os.getenv("GOPATH")
-	notify.info("Start to Check goctl binary")
-	if gopath == "" then
-		notify.error("%GOPATH not found")
-		return status
-	end
-	local goctl, _ = io.open(os.getenv("GOPATH") .. "/bin/goctl", "r")
-	if goctl ~= nil then
-		status = true
-		goctl:close()
-	end
-	return status
-end
 
 ---Check goctl environment
 function M.goctl_env()
